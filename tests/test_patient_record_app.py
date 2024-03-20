f0b98dc6c973bc5ca99e8fb61cc9c8033cbb56f1
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2024-03-20 15:33:02 -0400

    Added end-to-end test in test_patient_record_app.py

--- tests/test_patient_record_app.py
+++ tests/test_patient_record_app.py
@@ -1,5 +1,13 @@
 import unittest
+import subprocess
 
 class TestPatientRecordApp(unittest.TestCase):
-   def test_process(self):
-       pass
+    def setUp(self):
+        self.APP_FILE_PATH = 'src/patient_record_app.py'
+        self.INSTRUCTIONS_FILE_PATH = 'instructions/instructions.txt'
+        return super().setUp()
+    
+    def test_process(self):
+        actual_output = subprocess.run(['python', self.APP_FILE_PATH, self.INSTRUCTIONS_FILE_PATH], capture_output=True)
+        expected_output = 'Name: JOHN DOE, Id: 123, Exam Count: 0\nName: JANE CROW, Id: 789, Exam Count: 2\n'
+        self.assertEqual(actual_output.stdout.decode('utf-8'), expected_output)
