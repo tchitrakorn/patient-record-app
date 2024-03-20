0c61b8777b5bc35e50774ef2aa6746df5c56537f
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2024-03-20 14:45:03 -0400

    Implemented process_instruction() method in RecordProcessor

--- src/record_processor.py
+++ src/record_processor.py
@@ -31,7 +31,51 @@ class RecordProcessor:
         
         return: None
         """
-        pass
+        # break the instruction down into components
+        components = instruction.split(' ')
+        operation = components[0]
+        type = components[1]
+
+        # case 1: ADD PATIENT
+        if operation == 'ADD' and type == 'PATIENT':
+            patient_id = components[2]
+            patient_name = ' '.join(components[3:])
+            # edge case: only add if patient_id does not already exist
+            if patient_id not in self.ids_to_patients:
+                self.ids_to_patients[patient_id] = patient_name
+
+        # case 2: ADD EXAM
+        elif operation == 'ADD' and type == 'EXAM':
+            patient_id = components[2]
+            exam_id = components[3]
+            # edge case: only add if patient_id exists and exam_id does not already exist
+            if patient_id in self.ids_to_patients and exam_id not in self.exams_to_ids:
+                self.ids_to_exams[patient_id].append(exam_id)
+                self.exams_to_ids[exam_id] = patient_id
+
+        # case 3: DEL PATIENT
+        elif operation == 'DEL' and type == 'PATIENT':
+            patient_id = components[2]
+            # edge case: only delete if patient_id exists
+            if patient_id in self.ids_to_patients:
+                del self.ids_to_patients[patient_id]
+                # delete all exams affaliated with this patient_id if any
+                try:
+                    affiliated_exams = self.ids_to_exams[patient_id]
+                    del self.ids_to_exams[patient_id]
+                    for exam in affiliated_exams:
+                        del self.exams_to_ids[exam]
+                except:
+                    pass
+
+        # case 4: DEL EXAM
+        elif operation == 'DEL' and type == 'EXAM':
+            exam_id = components[2]
+            # edge case: only delete if exam_id exists
+            if exam_id in self.exams_to_ids:
+                patient_id = self.exams_to_ids[exam_id]
+                self.ids_to_exams[patient_id].remove(exam_id)
+                del self.exams_to_ids[exam_id]
 
     def aggregate_patient_info(self):
         """
