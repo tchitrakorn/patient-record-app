8af67757e977951bd3ebdbd49e91aa5a5e91f43e
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2024-03-20 12:13:45 -0400

    Partially implemented patient_record_app.py by reading in instructions file, initializing RecordProcess, and processing instructions one by one

--- src/patient_record_app.py
+++ src/patient_record_app.py
@@ -3,12 +3,18 @@ from record_processor import RecordProcessor
 
 if __name__ == '__main__':
     # get the instructions filename
+    filename = sys.argv[1]
 
     # read the instructions file
+    instructions_f = open(filename, 'r')
+    instructions = instructions_f.readlines()
 
     # initialize RecordProcessor object in order to process instructions
+    processor = RecordProcessor()
 
     # process the instructions one at a time
+    for instruction in instructions:
+        processor.process_instruction(instruction.strip())
 
     # print the formatted output after all instructions have been processed
     pass
